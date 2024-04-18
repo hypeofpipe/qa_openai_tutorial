48fb5d0a31b900ecc3395f4b29504535242fa3c6
Volodymyr Voleniuk <volodymyr.voleniuk@flip.shop>
2024-04-18 22:35:13 +0200

    improve data refiner: change directories paths

--- web_crawler/data_refiner.py
+++ web_crawler/data_refiner.py
@@ -43,30 +43,38 @@ def process_file(input_file_path, output_file_path):
 
     print(f"Processed and cleaned the file: {input_file_path}, result saved as: {output_file_path}")
 
-def toCSV(directory_path):
+def toCSV(directory_path_with_files, csv_file_path):
+    processed_csv_directory_path = csv_file_path
+    if not os.path.exists(processed_csv_directory_path):
+        os.mkdir(processed_csv_directory_path)
+    
     texts = []
-    for filename in os.listdir(directory_path):
+    for filename in os.listdir(directory_path_with_files):
         if filename.endswith('.txt'): 
-            file_path = os.path.join(directory_path, filename)
+            file_path = os.path.join(directory_path_with_files, filename)
             try:    
                 with open(file_path, 'r', encoding='utf-8') as file:
                     text = file.read()
                     
                     # Omit the first 11 lines and the last 4 lines, then replace special characters with spaces
-                    texts.append((text[11:-4].replace('-',' ').replace('_', ' ').replace(':',' '), clean_text(text)))
+                    texts.append((text[11:-4].replace('-',' ').replace('_', ' ').replace(':',' '), text))
             except Exception as e:
                     print(f"Error reading file: {file_path}, {e}. Ignoring this file.")
                     continue
                 
     df = pandas.DataFrame(texts, columns = ['fname', 'text'])
-    df.to_csv('processed/scraped.csv')
+    df.to_csv(f"{processed_csv_directory_path}/scraped.csv")
     df.head()
 
 def clean_directory(directory_path):
+    processed_directory_path = os.path.join(directory_path, 'processed')
+    if not os.path.exists(processed_directory_path):
+        os.mkdir(processed_directory_path)
+        
     for filename in os.listdir(directory_path):
         if filename.endswith('.txt'): 
             file_path = os.path.join(directory_path, filename)
-            output_file_path = os.path.join(f"{directory_path}_processed", filename)
+            output_file_path = os.path.join(processed_directory_path, filename)
             process_file(file_path, output_file_path)
 
 # Directory containing the text files
@@ -74,4 +82,4 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 directory_path = 'textzakon.rada.gov.ua copy'
 
 clean_directory(os.path.join(current_directory, directory_path))
-toCSV(os.path.join(current_directory, directory_path))
+toCSV(os.path.join(current_directory, directory_path, 'processed'), os.path.join(current_directory, 'processed'))
