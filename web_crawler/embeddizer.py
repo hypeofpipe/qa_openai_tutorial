8ebc79f3247c49f9c562a8ede38e652a84d06399
Volodymyr Voleniuk <volodymyr.voleniuk@flip.shop>
2024-04-19 00:55:57 +0200

    add embeddizer and tokenizer

---
+++ web_crawler/embeddizer.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+import pandas as pd
+import os
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+df = pd.read_csv('processed/shortened.csv', index_col=0)
+
+df['embeddings'] = df.text.apply(lambda x: client.embeddings.create(input=x, model='text-embedding-ada-002').data[0].embedding)
+
+df.to_csv('processed/embeddings.csv')
+df.head()
